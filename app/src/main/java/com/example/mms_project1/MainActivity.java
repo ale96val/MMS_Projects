14842f3437b7b7a9ed249dc3cc04d8aa0b8f9beb
Alejandro V <ale.valcarcel.ins@gmail.com>
2022-03-17 14:53:11 +0100

    Start.

--- app/src/main/java/com/example/mms_project1/MainActivity.java
+++ app/src/main/java/com/example/mms_project1/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mms_project1;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TAG", "The onCreate() event");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TAG", "The onStart() event");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TAG", "The onResume() event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TAG", "The onPause() event");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TAG", "The onStop() event");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG", "The onDestroy() event");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("TAG", "The onRestart() event");
     }
 }
