b6aaa4f6f749494ee9c1d05f631675ea07bf1733
Alejandro V <ale.valcarcel.ins@gmail.com>
2022-03-31 13:03:29 +0200

    Lab2.

--- Lab2/app/src/main/java/com/example/lab2/AddActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/AddActivity.java
@@ -1,12 +1,9 @@
 package com.example.lab2;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class AddActivity extends AppCompatActivity {
 
--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.lab2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addElement(View view) {
-        Snackbar.make(view, "Here's a Snack", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
     }
 }
