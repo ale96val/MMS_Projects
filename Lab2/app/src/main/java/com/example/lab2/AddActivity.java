34d7447c006eca2a72433373af78e08839456c6f
Alejandro V <ale.valcarcel.ins@gmail.com>
2022-04-06 18:42:04 +0200

    Lab2 Day2.

--- Lab2/app/src/main/java/com/example/lab2/AddActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/AddActivity.java
@@ -1,12 +1,26 @@
 package com.example.lab2;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddActivity extends AppCompatActivity {
 
+    String TaskAdded = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +53,26 @@ public class AddActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
+    }
+
+    public void add_task(View view) {
+        EditText Name = findViewById(R.id.TaskNameText);
+        String NameText = Name.getText().toString();
+        EditText Description = findViewById(R.id.task_description_text);
+        String DescriptionText = Description.getText().toString();
+        RadioGroup TaskType = findViewById(R.id.select_task);
+        int TaskType2 = TaskType.getCheckedRadioButtonId();
+        RadioButton radioButton = findViewById(TaskType2);
+        String TaskTypeText = radioButton.getText().toString();
+        EditText TaskDate= findViewById(R.id.task_date);
+        String TaskDateText = TaskDate.getText().toString();
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch Status = findViewById(R.id.Status_text);
+        boolean StatusText = Status.isChecked();
+        TaskAdded = NameText + ", " + DescriptionText + ", " + TaskTypeText + ", " + TaskDateText + ", " + StatusText;
+        SharedPreferences sharedPref = getSharedPreferences("Task", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("Task", TaskAdded);
+        editor.commit();
+        finish();
     }
 }
--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.lab2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Set<String> StringSet = new HashSet<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
     }
 
     protected void onResume() {
@@ -25,7 +31,19 @@ public class MainActivity extends AppCompatActivity {
     }
     protected void onStart() {
         super.onStart();
-
+        SharedPreferences sharedPref = getSharedPreferences("Task",Context.MODE_PRIVATE);
+        String task = sharedPref.getString("Task", "default");
+        if(task!="default"){
+            StringSet.add(task);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.clear();
+            editor.commit();
+            SharedPreferences sharedPref2 = getSharedPreferences("Tasks",Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor2 = sharedPref2.edit();
+            editor2.putStringSet("Tasks", StringSet);
+            Log.d("TAG", StringSet.toString());
+            editor2.commit();
+        }
     }
     protected void onRestart() {
         super.onRestart();
