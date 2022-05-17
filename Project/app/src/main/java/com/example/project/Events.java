28e6ccb8ef3006426d1ed4e7c1308d3a217a02b3
Alejandro V <ale.valcarcel.ins@gmail.com>
2022-05-17 18:54:54 +0200

    Day 2.

--- Project/app/src/main/java/com/example/project/Events.java
+++ Project/app/src/main/java/com/example/project/Events.java
@@ -8,12 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
 
